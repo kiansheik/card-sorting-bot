a1e8482ab97696d3fd0823e4ca35aba00ed226bf
Kian Sheik <ksheik@referralexchange.com>
2022-12-21 17:44:10 -0800

    added firmware settings for when I get a new board; i ordered a new one last night that supports 3 axis because the one I have I found out is 2 axis with one double motored. I ordered another one so I can start to get a hang of the Z axis controls. I added a list in the `read_cache` for getting the top value of a stack so in theory once every stack has had every element moved at some point, the read_cache will basically be a running state of the machine and we won't need to perform any reads after we know where everything is, saving us time. we will want to add a `been_emptied` flag to a stack which triggers true after is_empty has returned true once for this stack. We can assume to know all of the elements inside of a stack, whether it is empty and whether it is full, once the been_emptied flag has been triggered true. I think we can really greatly reduce the amount of reads with this.

--- sorting/card_sort_algo.py
+++ sorting/card_sort_algo.py
@@ -2,7 +2,7 @@ import random
 from math import ceil, sqrt, floor
 
 MOVE_DURATION = 5
-READ_DURATION = 1
+READ_DURATION = 2
 
 
 def rectangle_dimensions(area):
@@ -149,7 +149,7 @@ class Pile:
         self.group_map = dict()
         self.reset_movement_counters()
         self.read_cache = {
-            i: {"empty": None, "full": None, "top": None} for i in range(self.length())
+            i: {"empty": None, "full": None, "top": []} for i in range(self.length())
         }
 
     def reset_movement_counters(self):
@@ -157,35 +157,64 @@ class Pile:
         self.uncached_reads = 0
         self.cached_reads = 0
 
-    def get_stack(self, stack_idx):
-        return self.group_map.get(stack_idx, self.stacks[stack_idx])
+    def get_stack(self, stack_idx, idx=None):
+        if ".stack[" in str(stack_idx):
+            vals = str(stack_idx).split(".stack[")
+            stack_idx = vals[0]
+            idx = int(vals[1].split("]")[0])
+        stack = self.group_map.get(str(stack_idx), self.stacks[int(stack_idx)])
+        if idx is None:
+            return stack
+        # Case when we want to directly access a Stack Group's element stack
+        return stack.stacks[idx]
 
     def put_stack(self, stack):
         self.stacks.append(stack)
-        stack_idx = self.length() - 1
+        stack_idx = str(self.length() - 1)
         self.put_read_cache(stack_idx, "empty", None)
         self.put_read_cache(stack_idx, "full", None)
-        self.put_read_cache(stack_idx, "top", None)
+        self.put_read_cache(stack_idx, "top", [])
 
     def put_stack_group(self, stack, stack_idx):
+        stack_idx = str(stack_idx)
         self.group_map[stack_idx] = stack
         self.put_read_cache(stack_idx, "empty", None)
         self.put_read_cache(stack_idx, "full", None)
-        self.put_read_cache(stack_idx, "top", None)
+        self.put_read_cache(stack_idx, "top", [])
+
+    def swap(self):
+        return self.stacks[-1]
+
+    def swap_idx(self):
+        return self.length() - 1
 
     def get_read_cache(self, stack_idx, name):
         stack = self.get_stack(stack_idx)
         if type(stack) == StackGroup:
             stack_idx = f"{stack_idx}_group"
-        return self.read_cache[stack_idx][name]
+        rc = self.read_cache.get(stack_idx, {"empty": None, "full": None, "top": []})[
+            name
+        ]
+        if name == "top":
+            return rc[-1] if len(rc) > 0 else None
+        else:
+            return rc
 
-    def put_read_cache(self, stack_idx, name, val):
-        stack = self.get_stack(stack_idx)
+    def put_read_cache(self, stack_idx, name, val, idx=None):
+        stack = self.get_stack(stack_idx, idx=idx)
         if type(stack) == StackGroup:
             stack_idx = f"{stack_idx}_group"
+        stack_idx = str(stack_idx)
         if stack_idx not in self.read_cache.keys():
-            self.read_cache[stack_idx] = {"empty": None, "full": None, "top": None}
-        self.read_cache[stack_idx][name] = val
+            self.read_cache[stack_idx] = {"empty": None, "full": None, "top": []}
+        if name == "top":
+            if val is None:
+                if len(self.read_cache[stack_idx][name]) > 0:
+                    self.read_cache[stack_idx][name].pop()
+            else:
+                self.read_cache[stack_idx][name].append(val)
+        else:
+            self.read_cache[stack_idx][name] = val
 
     def comparator(self, a, b):
         return a < b
@@ -196,51 +225,59 @@ class Pile:
     def size(self):
         return sum([len(stack) for stack in self.stacks])
 
-    def read_top_element(self, stack_idx):
+    def read_top_element(self, stack_idx, sg_idx=None):
+        if sg_idx is not None:
+            stack_idx = f"{stack_idx}.stack[{sg_idx}]"
         top = self.get_read_cache(stack_idx, "top")
         if top is not None:
             self.cached_reads += 1
             return top
-        top = self.get_stack(stack_idx).read_top_element()
+        top = self.get_stack(stack_idx, idx=sg_idx).read_top_element()
         self.put_read_cache(stack_idx, "top", top)
         if top is not None:
             self.put_read_cache(stack_idx, "empty", False)
         self.uncached_reads += 1
         return top
 
-    def is_full(self, stack_idx):
+    def is_full(self, stack_idx, sg_idx=None):
+        if sg_idx is not None:
+            stack_idx = f"{stack_idx}.stack[{sg_idx}]"
         full = self.get_read_cache(stack_idx, "full")
         if full is not None:
             self.cached_reads += 1
             return full
-        full = self.get_stack(stack_idx).is_full()
+        full = self.get_stack(stack_idx, idx=sg_idx).is_full()
         self.put_read_cache(stack_idx, "full", full)
         if full:
             self.put_read_cache(stack_idx, "empty", False)
         self.uncached_reads += 1
         return full
 
-    def is_empty(self, stack_idx):
+    def is_empty(self, stack_idx, sg_idx=None):
+        if sg_idx is not None:
+            stack_idx = f"{stack_idx}.stack[{sg_idx}]"
         empty = self.get_read_cache(stack_idx, "empty")
         if empty is not None:
             self.cached_reads += 1
             return empty
-        empty = self.get_stack(stack_idx).is_empty()
+        empty = self.get_stack(stack_idx, idx=sg_idx).is_empty()
         self.put_read_cache(stack_idx, "empty", empty)
         if empty:
             self.put_read_cache(stack_idx, "full", False)
         self.uncached_reads += 1
         return empty
 
-    def move_element(self, src_stack, dest_stack):
-        elem = self.get_stack(src_stack).pop()
-        self.get_stack(dest_stack).push(elem)
-
+    def move_element(self, src_stack, dest_stack, src_idx=None, dest_idx=None):
+        elem = self.get_stack(src_stack, idx=src_idx).pop()
+        self.get_stack(dest_stack, idx=dest_idx).push(elem)
+        if src_idx is not None:
+            src_stack = f"{src_stack}.stack[{src_idx}]"
         # Update read cache with values we can assume
         self.put_read_cache(src_stack, "full", False)
         self.put_read_cache(src_stack, "empty", None)
         self.put_read_cache(src_stack, "top", None)
-
+        if dest_idx is not None:
+            dest_stack = f"{dest_stack}.stack[{dest_idx}]"
         self.put_read_cache(dest_stack, "full", None)
         self.put_read_cache(dest_stack, "empty", False)
         self.put_read_cache(dest_stack, "top", elem)
@@ -275,41 +312,107 @@ class Pile:
 
     def sort(self):
         # Merge two sorted stacks into a single sorted stack
-        def merge(stack1_idx, stack2_idx, swap_idx):
+        def merge(stack1_idx, stack2_idx, swap_idx, pile=self):
             num_elems = 0
-            while not self.is_empty(stack1_idx) and not self.is_empty(stack2_idx):
-                if self.comparator(
-                    self.read_top_element(stack1_idx), self.read_top_element(stack2_idx)
+            while not pile.is_empty(stack1_idx) and not pile.is_empty(stack2_idx):
+                if pile.comparator(
+                    pile.read_top_element(stack1_idx), pile.read_top_element(stack2_idx)
                 ):
-                    self.move_element(stack1_idx, swap_idx)
+                    pile.move_element(stack1_idx, swap_idx)
                 else:
-                    self.move_element(stack2_idx, swap_idx)
+                    pile.move_element(stack2_idx, swap_idx)
                 num_elems += 1
             # Move remaining elements from stack1 to merged_stack
-            while not self.is_empty(stack1_idx):
-                self.move_element(stack1_idx, swap_idx)
+            while not pile.is_empty(stack1_idx):
+                pile.move_element(stack1_idx, swap_idx)
                 num_elems += 1
             # Move remaining elements from stack2 to merged_stack
-            while not self.is_empty(stack2_idx):
-                self.move_element(stack2_idx, swap_idx)
+            while not pile.is_empty(stack2_idx):
+                pile.move_element(stack2_idx, swap_idx)
                 num_elems += 1
             # Create a StackGroup with stack1 and stack2, then replace stack1 inside of the pile with this group
             stack_group = StackGroup(
-                self.get_stack(stack1_idx), self.get_stack(stack2_idx)
+                pile.get_stack(stack1_idx), pile.get_stack(stack2_idx)
             )
-            self.group_map[stack1_idx] = stack_group
-            # Move elements from merged_stack back to stack1 and stack2 in reverse order
+            pile.group_map[str(stack1_idx)] = stack_group
+            # Move elements from merged_stack back to stack1 in reverse order
             for i in range(num_elems):
-                self.move_element(swap_idx, stack1_idx)
+                pile.move_element(swap_idx, stack1_idx)
 
         # Divide and merge the stacks until they are sorted
-        def divide_and_merge(start_idx, end_idx, swap_idx):
+        def divide_and_merge(start_idx, end_idx, swap_idx, pile=self):
             if start_idx == end_idx:
                 return
             mid = (start_idx + end_idx) // 2
-            divide_and_merge(start_idx, mid, swap_idx)
-            divide_and_merge(mid + 1, end_idx, swap_idx)
-            merge(start_idx, mid + 1, swap_idx)
+            divide_and_merge(start_idx, mid, swap_idx, pile=pile)
+            divide_and_merge(mid + 1, end_idx, swap_idx, pile=pile)
+            merge(start_idx, mid + 1, swap_idx, pile=pile)
+
+        def sort_stack(stack_idx):
+            # Buffer only needs to hold one car, like a variable, so we can use any other stack in the pile
+            # This ensures it will be a stack other than the two being used. This assumes that a stack always has at least
+            # 1 less than the maximum card value
+
+            # Find a buffer stack to use which is not full and not in a group
+            # This stack can be any stack because it is used to store only 1 card at a time
+            swap_idx = self.swap_idx()
+            for i in range(swap_idx):
+                if stack_idx != i:
+                    if not self.get_stack(i).in_group() and not self.is_full(i):
+                        buffer_idx = i
+                        break
+            while not self.is_empty(stack_idx):
+                if not self.is_empty(swap_idx) and self.comparator(
+                    self.read_top_element(stack_idx), self.read_top_element(swap_idx)
+                ):
+                    self.move_element(stack_idx, buffer_idx)
+                    self.move_element(swap_idx, stack_idx)
+                    while not self.is_empty(swap_idx) and self.comparator(
+                        self.read_top_element(buffer_idx),
+                        self.read_top_element(swap_idx),
+                    ):
+                        self.move_element(swap_idx, stack_idx)
+                    self.move_element(buffer_idx, swap_idx)
+                self.move_element(stack_idx, swap_idx)
+            # Move elements from swap back to stack in reverse order
+            while not self.is_empty(swap_idx):
+                self.move_element(swap_idx, stack_idx)
+
+        def sort_stack_immproved(stack_idx):
+            # We want to sort `stack` using the list of `swap_stacks` as space to sort
+            src_stack = self.get_stack(stack_idx)
+            swap_idx = self.swap_idx()
+            swap_stacks = self.swap().stacks
+
+            # The state will help us keep track of where we put each element from `stack`
+            state = [list() for _ in swap_stacks]
+            while not src_stack.is_empty():
+                elem = src_stack.read_top_element()
+                # Leave last stack for unsortable elements
+                for i in range(len(state) - 1):
+                    if len(state[i]) == 0 or not self.comparator(elem, state[i][-1]):
+                        # Keep track internally
+                        state[i].append(elem)
+                        # Move element on the robot from one stack to another
+                        self.move_element(stack_idx, swap_idx, dest_idx=i)
+                        # Let rest of loop know that we have placed elem and break
+                        elem = None
+                        break
+                if elem is not None:
+                    state[len(state) - 1].append(elem)
+                    self.move_element(stack_idx, swap_idx, dest_idx=len(state) - 1)
+            tl = Pile(state)
+            print(tl)
+            while sum([len(stack) for stack in state[:-1]]) > 0:
+                min_val = None
+                for i, stack in enumerate(state[:-1]):
+                    if len(stack) > 0 and (
+                        min_val is None or not self.comparator(stack[-1], min_val[1])
+                    ):
+                        min_val = (i, stack.pop())
+                self.move_element(swap_idx, stack_idx, src_idx=min_val[0])
+            print(src_stack)
+            breakpoint()
 
         self.group_map = dict()
         self.reset_movement_counters()
@@ -325,7 +428,8 @@ class Pile:
         last_stack_idx = num_stacks - 2
         # Sort each stack up to the swap
         for stack_idx in range(swap_idx):
-            self.sort_stack(stack_idx, swap_idx)
+            sort_stack(stack_idx)
+        print(self.total_moves, self.uncached_reads)
         # Merge each sorted stack
         divide_and_merge(0, last_stack_idx, swap_idx)
         # Remove the empty stack from the end of the pile
@@ -343,39 +447,11 @@ class Pile:
         )
         self.reset_movement_counters()
 
-    def sort_stack(self, stack_idx, swap_idx):
-        # Buffer only needs to hold one car, like a variable, so we can use any other stack in the pile
-        # This ensures it will be a stack other than the two being used. This assumes that a stack always has at least
-        # 1 less than the maximum card value
-
-        # Find a buffer stack to use which is not full and not in a group
-        # This stack can be any stack because it is used to store only 1 card at a time
-        for i in range(swap_idx):
-            if stack_idx != i:
-                if not self.get_stack(i).in_group() and not self.is_full(i):
-                    buffer_idx = i
-                    break
-        while not self.is_empty(stack_idx):
-            if not self.is_empty(swap_idx) and self.comparator(
-                self.read_top_element(stack_idx), self.read_top_element(swap_idx)
-            ):
-                self.move_element(stack_idx, buffer_idx)
-                self.move_element(swap_idx, stack_idx)
-                while not self.is_empty(swap_idx) and self.comparator(
-                    self.read_top_element(buffer_idx), self.read_top_element(swap_idx)
-                ):
-                    self.move_element(swap_idx, stack_idx)
-                self.move_element(buffer_idx, swap_idx)
-            self.move_element(stack_idx, swap_idx)
-        # Move elements from swap back to stack in reverse order
-        while not self.is_empty(swap_idx):
-            self.move_element(swap_idx, stack_idx)
-
 
 p = Pile(
     [
         [ceil(random.random() * 30_000) for _ in range(ceil((Stack.max_size - 1)))]
-        for _ in range(5 * 5)
+        for _ in range(4 * 2)
     ]
 )
 print(p, "\n")
