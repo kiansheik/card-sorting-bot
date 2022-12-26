15dd17aba8e09227e5b91cc0387d393d931e9238
Kian Sheik <ksheik@referralexchange.com>
2022-12-26 12:36:35 -0800

    added backgrounds

--- vision/scale_background.py
+++ vision/scale_background.py
@@ -5,8 +5,10 @@ import imgaug as ia
 import imgaug.augmenters as iaa
 import imutils
 import numpy as np
+import os
 import pandas as pd
 from PIL import Image
+from tqdm import tqdm
 
 
 def load_annotations(json_path):
@@ -19,6 +21,17 @@ def load_annotations(json_path):
         >= pd.Timestamp("2022-12-24 21:14:36.599512+0000", tz="UTC")
     ]
     return annotations
+
+
+def get_file_paths(dir_path):
+    file_paths = []
+    for root, directories, files in os.walk(dir_path):
+        for file in files:
+            if not file.startswith("."):
+                file_path = os.path.join(root, file)
+                file_path = os.path.abspath(file_path)
+                file_paths.append(file_path)
+    return file_paths
 
 
 def rotate_bound(image, angle, bounding_boxes):
@@ -42,10 +55,9 @@ def rotate_bound(image, angle, bounding_boxes):
     )
     # apply the rotation transformation to the mask
     mask = cv2.warpAffine(mask, M, (w, h))
-    boxes = []
-    for i, bounding_box in enumerate(bounding_boxes):
+    boxes = dict()
+    for label, bounding_box in bounding_boxes.items():
         box_x, box_y, box_w, box_h = [int(x) for x in bounding_box]
-        print(bounding_box)
         bbox_mask = np.zeros((h, w), dtype=np.uint8)
         cv2.rectangle(
             bbox_mask,
@@ -55,7 +67,7 @@ def rotate_bound(image, angle, bounding_boxes):
             -1,
         )
         bbox_mask = cv2.warpAffine(bbox_mask, M, (w, h))
-        boxes.append(bbox_mask)
+        boxes[label] = bbox_mask
     # return both the rotated image and the mask
     return rotated_image, mask, boxes
 
@@ -77,7 +89,14 @@ def bitmask_to_bounding_box(bitmask):
 
 
 def overlay_image(
-    foreground, background, scale_factor=0.5, x=0.5, y=0.5, angle=0.5, bounding_boxes=[]
+    foreground,
+    background,
+    scale_factor=0.5,
+    x=0.5,
+    y=0.5,
+    angle=0.5,
+    bounding_boxes=dict(),
+    draw_boxes=False,
 ):
     foreground_scaled, mask, box_masks = rotate_bound(
         foreground, 360 * angle, bounding_boxes
@@ -108,8 +127,8 @@ def overlay_image(
 
     # Overlay the scaleed foreground image on the background image
     mask_inv = cv2.bitwise_not(mask)
-    final_boxes = []
-    for box_mask in box_masks:
+    final_boxes = dict()
+    for label, box_mask in box_masks.items():
         box_mask = ia.imresize_single_image(
             box_mask,
             (
@@ -122,7 +141,7 @@ def overlay_image(
             x : foreground_scaled.shape[0] + x, y : foreground_scaled.shape[1] + y
         ] = box_mask
         bounding_box = bitmask_to_bounding_box(bg_mask)
-        final_boxes.append(bounding_box)
+        final_boxes[label] = bounding_box
     masked_foreground = cv2.bitwise_and(foreground_scaled, foreground_scaled, mask=mask)
     masked_background = cv2.bitwise_and(
         background[
@@ -138,50 +157,67 @@ def overlay_image(
     ] = cv2.add(masked_foreground, masked_background)
     # Convert the bitmask to a bounding box
     # Draw the bounding box on the image as a green rectangle
-    for bounding_box in final_boxes:
-        cv2.drawContours(background, [bounding_box], -1, (0, 255, 0), 2)
+    if draw_boxes:
+        for bounding_box in final_boxes.values():
+            cv2.drawContours(background, [bounding_box], -1, (0, 255, 0), 2)
     return background, final_boxes
 
 
-background = cv2.imread("/Users/kiansheik/Downloads/table_bg.jpeg")
-annotations = load_annotations(
-    "/Users/kiansheik/code/card-sorting-bot/vision/annotations/project-3-at-2022-12-25-00-09-f9c60779.json"
-)
-annotated_ids = {
-    x["data"]["image"].split("/")[-1].split(".")[0]: x for x in annotations
-}
+def generate_augmented_dataset(
+    output_path, input_path, background_path, annotations_path, images_per_background=10
+):
+    annotations = load_annotations(annotations_path)
+    annotated_ids = {
+        x["data"]["image"].split("/")[-1].split(".")[0]: x for x in annotations
+    }
+    new_annotations = dict()
+    background_path_list = get_file_paths(background_path)
+    tq = tqdm(
+        desc="Augmenting files...",
+        total=len(background_path_list) * len(annotated_ids) * images_per_background,
+    )
+    for card_id, annotation in annotated_ids.items():
+        for bp in background_path_list:
+            background = cv2.imread(bp)
+            background_name = bp.split("/")[-1].replace(".", "_")
+            for i in range(images_per_background):
+                # Extraxt bounding boxes from annotations to be transformed
+                notes = annotation["annotations"][-1]["result"]
+                boxes = {
+                    x["value"]["rectanglelabels"][0]: (
+                        x["value"]["x"] / 100.0 * x["original_width"],
+                        x["value"]["y"] / 100.0 * x["original_height"],
+                        x["value"]["width"] / 100.0 * x["original_width"],
+                        x["value"]["height"] / 100.0 * x["original_height"],
+                    )
+                    for x in notes
+                }
+                res, bboxes = overlay_image(
+                    cv2.imread(f"{input_path}/{card_id}.jpg"),
+                    background.copy(),
+                    scale_factor=random.uniform(0.75, 1),
+                    x=random.random(),
+                    y=random.random(),
+                    angle=random.uniform(0, 1),
+                    bounding_boxes=boxes,
+                )
+                # Save the image to a file
+                out_path = f"{output_path}/{card_id}_{background_name}_{i}.jpg"
+                cv2.imwrite(out_path, res)
+                new_annotations[card_id] = {
+                    "bbox": bboxes,
+                    "card_id": card_id,
+                    "file": out_path,
+                }
+                tq.update(1)
+    with open(f"{output_path}/annotations.json", "w") as f:
+        json.dump(new_annotations, f)
+    print(f"Augmented data generated in {output_path}")
 
-with open("/Users/kiansheik/Downloads/oracle-cards-20221222220256.json") as f:
-    all_cards = json.load(f)
 
-i = 0
-for card in all_cards:
-    if card["id"] in annotated_ids:
-        # Extraxt bounding boxes from annotations to be transformed
-        notes = annotated_ids[card["id"]]["annotations"][-1]["result"]
-        boxes = [
-            (
-                x["value"]["x"] / 100.0 * x["original_width"],
-                x["value"]["y"] / 100.0 * x["original_height"],
-                x["value"]["width"] / 100.0 * x["original_width"],
-                x["value"]["height"] / 100.0 * x["original_height"],
-            )
-            for x in notes
-        ]
+output_path = "/Users/kiansheik/code/card-sorting-bot/vision/augmented_data"
+input_path = "/Users/kiansheik/code/card-sorting-bot/vision/data"
+background_path = "/Users/kiansheik/code/card-sorting-bot/vision/backgrounds"
+annotations_path = "/Users/kiansheik/code/card-sorting-bot/vision/annotations/project-3-at-2022-12-25-00-09-f9c60779.json"
 
-        res, bboxes = overlay_image(
-            cv2.imread(f"vision/data/{card['id']}.jpg"),
-            background,
-            scale_factor=random.uniform(0.75, 1),
-            x=random.random(),
-            y=random.random(),
-            angle=random.uniform(0, 1),
-            bounding_boxes=boxes,
-        )
-        # Display the augmented image using OpenCV
-        cv2.imshow("Augmented Image", res)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        if i > 10:
-            break
-        i += 1
+generate_augmented_dataset(output_path, input_path, background_path, annotations_path)
